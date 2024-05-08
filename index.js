08e80e7f891ad6b66a84aebd383138cd2f79fa24
tuantien342 <kamilsahannam1311@outlook.com>
2024-05-08 17:20:34 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const last = (arr) => arr[arr.length - 1];
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
+  let max = 1;
+  for (let i = 1; i < envelopes.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (envelopes[i][1] > envelopes[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        max = Math.max(max, dp[i]);
+      }
+    }
+  }
+  return max;
+}
