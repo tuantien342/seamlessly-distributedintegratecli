75f627e228bef472b287370740b243fe1f87a643
tuantien342 <kamilsahannam1311@outlook.com>
2024-05-13 18:04:41 +0700

    update task

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const originalObject = { name: "Alice", age: 30 };
-const clonedObject = JSON.parse(JSON.stringify(originalObject));
-console.log(clonedObject); // { name: "Alice", age: 30 }
+function mergeKLists(lists) {
+  if (lists.length === 0) return null;
+  while (lists.length > 1) {
+    const first = lists.shift();
+    const second = lists.shift();
+    const merged = mergeTwoLists(first, second);
+    lists.push(merged);
+  }
+  return lists[0];
+}
