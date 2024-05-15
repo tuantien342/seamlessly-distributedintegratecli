9ed0bed100d6ab9f8c93c7a4225b82b19bf63f37
tuantien342 <kamilsahannam1311@outlook.com>
2024-05-15 11:26:46 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function mergeKLists(lists) {
-  if (lists.length === 0) return null;
-  while (lists.length > 1) {
-    const first = lists.shift();
-    const second = lists.shift();
-    const merged = mergeTwoLists(first, second);
-    lists.push(merged);
+function maxSlidingWindow(nums, k) {
+  const result = [];
+  const queue = [];
+  for (let i = 0; i < nums.length; i++) {
+    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
+      queue.pop();
+    }
+    queue.push(i);
+    if (queue[0] === i - k) queue.shift();
+    if (i >= k - 1) result.push(nums[queue[0]]);
   }
-  return lists[0];
+  return result;
 }
